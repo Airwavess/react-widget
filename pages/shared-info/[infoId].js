4dccc1f5cdb6f02bb493d16d906e70b6a156081a
Airwaves <prairie5270@gmail.com>
2020-07-16 20:45:10 +0800

    Change API routes

--- pages/shared-info/[infoId].js
+++ pages/shared-info/[infoId].js
@@ -25,9 +25,9 @@ export async function getStaticProps({ params }) {
   const id = params.infoId;
   let api;
   if (id.includes("image")) {
-    api = `${API_HOST}/api/v1/image/${id}`;
+    api = `${API_HOST}/api/v1/images/${id}`;
   } else if (id.includes("video")) {
-    api = `${API_HOST}/api/v1/video/${id}`;
+    api = `${API_HOST}/api/v1/videos/${id}`;
   }
   const res = await axios.get(api);
   const data = res.data;
